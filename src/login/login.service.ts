71dbf7aa9e151cc93ababee460baf49b5875a1dd
xueke-alan <xueke.alan@gmail.com>
2023-08-04 00:59:23 +0800

    8.4

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,12 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateLoginDto } from './dto/create-login.dto';
-import { UpdateLoginDto } from './dto/update-login.dto';
 import { UserPassword } from '../entities/UserPassword';
 import { User } from '../entities/User';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
-import { error, log } from 'console';
 
 @Injectable()
 export class LoginService {
@@ -22,7 +19,10 @@ export class LoginService {
     // 只查询salt，saltRounds，validUntil
     const user = await this.userPassword.findOne({ where: { staffId } });
     if (user) {
-      return user;
+      return {
+        ...user,
+        preToken: this.jwtService.sign({ ...user }, { expiresIn: '3s' }),
+      };
     } else {
       throw new HttpException(
         {
@@ -34,32 +34,54 @@ export class LoginService {
     }
   }
 
-  // 改名字为validateUser??
   async login(HashPassword) {
-    // 这里应该联合查询，因为需要返回信息
+    const decodedToken = this.jwtService.decode(HashPassword.preToken);
+    // 超时验证，防止有人伪造prelogin
+    if (Date.now() - decodedToken['exp'] * 1000 > 0) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: '非法访问或访问超时',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
 
+    // 联合查询，需要返回信息
     const user = await this.User.findOne({
       where: { staffId: HashPassword.staffId },
       relations: ['userPasswords'],
     });
+    if (!user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          error: '账号有误',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
 
+    // 获取联合查询的userPasswords关系表信息
     const user_Password = user.userPasswords[0];
+    if (!user_Password) {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          error: '账号有误',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
 
-    // const verify = true;
-    const verify = HashPassword.toVerify == user_Password.hashPassword;
+    // 验证一致性
+    const verify = HashPassword.toVerify === user_Password.hashPassword;
 
     if (verify) {
-      console.log(HashPassword);
-
-      // 这里应该校验是否HashPassword.toUpdate一致，理论上是不用的因为前端的算法没问题就没问题，
-      // 但是防止api调用方式，所以要验证，或者禁止api调用，或者当prelogin的时候发送一个token，
-      // token有效期只有3s，通过这个token验证
-
+      // 重置盐值，迭代次数，hash密码
       this.userPassword.update(user_Password.id, HashPassword.toUpdate);
-
+      // 传回基本信息
       const payload = { staffId: user.staffId, usernameCn: user.usernameCn };
-
-      // 这里不需要返回全部信息，返回payload即可
       return {
         ...payload,
         token: this.jwtService.sign(payload),
